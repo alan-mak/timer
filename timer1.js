c37b11623f5be721d2a4434e1f0e7cd9d0ad06ba
Alan <alanmak95@gmail.com>
2020-12-01 19:45:39 +0000

    Solved with edge cases

--- timer1.js
+++ timer1.js
@@ -2,9 +2,23 @@ let alarmClock = function(arr) {
   if (arr === []) {
     return;
   }
-  arrSorted = arr.sort((a, b) => a-b)
-  console.log(arrSorted.length)
- 
+  let arrSorted = arr.sort((a, b) => a-b)
+  // console.log(arrSorted[arrSorted.length -1])
+
+  for (let counter of arrSorted) {
+    if (counter < 0) {
+      console.log(counter, "Not allowed in the past")
+    } else if (isNaN(counter)) {
+      console.log(counter, "is not a number type")
+    } else {
+      setTimeout(() => {
+          process.stdout.write('beep' );
+          setTimeout(() => {
+            console.log("\n")
+          })
+      }, counter  * 1000);
+    }
+  }
 };
 
 let alarm = process.argv.slice(2);
