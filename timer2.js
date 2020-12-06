585167f4175a87a94f51a2c8237a398c5c8c4544
Alan <alanmak95@gmail.com>
2020-12-06 18:40:52 -0500

    Second Timer With new options:

--- timer2.js
+++ timer2.js
@@ -3,26 +3,28 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let logic = true;
+//Used for encoding input from keyboard
+rl.input.setRawMode(true);
+rl.input.setEncoding('utf8');
 
-rl.question('Set for how many seconds?', (time) => {
+rl.input.on('data', time => {
   if (isNaN(time)) {
-    if(time === 'b') {
-        console.log('beep')
-        rl.close();
+    if (time === 'b') {
+      console.log('\nBeep')
+      rl.close();
+    } else if (time === '\u0003') {
+      rl.output.write("\nThanks for using me, ciao!\n");
+      process.exit();
     } else {
-        console.log('this is not a valid command')
-        rl.close();
+      console.log('\nThis is not a valid command')
+      rl.close();
     }
   } else {
-    console.log(`timer has been set for ${time} seconds`)
+    console.log(`\nTimer has been set for ${time} seconds`)
     setTimeout(() => {
       console.log('beep');
-    }, time  * 1000);
+    }, time * 1000);
     rl.close();
   }
-})
-// if (key === '\u0003') {
-  // console.log("Thanks for using me, ciao!");
-  // process.exit();
-// }
+});
+
