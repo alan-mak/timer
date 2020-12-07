970e629c68e6f954ef69d0b2101816c8ca787f60
Alan <alanmak95@gmail.com>
2020-12-07 02:11:07 +0000

    Eslint Check

--- timer2.js
+++ timer2.js
@@ -10,21 +10,20 @@ rl.input.setEncoding('utf8');
 rl.input.on('data', time => {
   if (isNaN(time)) {
     if (time === 'b') {
-      console.log('\nBeep')
+      console.log('\nBeep');
       rl.close();
     } else if (time === '\u0003') {
       rl.output.write("\nThanks for using me, ciao!\n");
       process.exit();
     } else {
-      console.log('\nThis is not a valid command')
+      console.log('\nThis is not a valid command');
       rl.close();
     }
   } else {
-    console.log(`\nTimer has been set for ${time} seconds`)
+    console.log(`\nTimer has been set for ${time} seconds`);
     setTimeout(() => {
       console.log('beep');
     }, time * 1000);
     rl.close();
   }
 });
-
